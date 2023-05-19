daa61c925e94676f28b98d6d9d235a2353910403
Personal <faisaltez@gmail.com>
2023-05-20 00:51:22 +0600

    first commit

---
+++ lib/8b.class_restaurant.dart
@@ -0,0 +1,13 @@
+import 'package:ostad_flutter_batch_practice/8a.class_restaurant.dart';
+
+void main(){
+  Restaurant kfc = Restaurant(4588552);
+
+  print(kfc.order("Burger"));
+  // kfc._prepareItem('adsasd');
+
+  // Printing a getters
+  print(kfc.resId);
+
+
+}
---
+++ test/ostad_flutter_batch_practice_test.dart
@@ -0,0 +1,8 @@
+import 'package:ostad_flutter_batch_practice/ostad_flutter_batch_practice.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
